31d2e139a602846027ebfdfe41e8345695094f17
Raxoria <syelvie17@gmail.com>
2021-01-30 17:22:47 +0700

    update 1

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct
+{
+	char nama[50];
+	char simbol;
+	bool isKomputer;
+	long long int skor;
+	int langkah[50];
+	int input;
+}Pemain;
+
+typedef struct 
+{
+	int ukuran;
+	int waktu;
+	int kesulitan;
+	int giliran;
+	bool isBermain;
+	char isi[10][10];
+	Pemain pemain[2];
+}Papan;
+
+Papan papan;
+
 
 int main(int argc, char *argv[])
 {
