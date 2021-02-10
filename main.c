c768d9f2adfdd186bdd8fb991232da991bfa5134
Raxoria <syelvie17@gmail.com>
2021-02-10 21:27:06 +0700

    a

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
+#include <ctype.h>
+#include <math.h>
+
+#define KEY_ENTER 13
+#define KEY_ESCAPE 27
 
 typedef struct
 {
@@ -24,14 +32,275 @@ typedef struct
 }Papan;
 
 Papan papan;
+bool validInput = true;
+void menuUtama();
+void mulaiPermainan();
+void modeMultiPemain();
+void modeKomputer();
+void bagianSkor();
+void bagianCaraBermain();
+void bagianKeluar();
+void simpanSkor();
+void bagianPermainan();
+void pesanInvalid(char* pesan);
+void menuPanel();
+void menuOpsi();
+void menuPilihan();
+void pilihan();
+void aturPermainan();
+void pilihanYaTidak();
+void tampilSkor();
+
+void biarkan(){}
+
+void tampilSkor()
+{
+	FILE *in;
+	char tampil[6000];
+	
+	if((in=fopen("D:\\Kuliah\\Semester 1\\Dasar Dasar Pemograman\\Tugas Besar\\TicTacToe-Game\\Daftar10SkorTertinggi1.txt", "r")) == NULL){
+		printf("Error Opening File"); //tutup program karena file ga ada
+		fclose(in);
+	}
+		//baca isi file dengan gets lalu simpan ke tampil
+	while(fgets(tampil, sizeof(tampil), in)){
+		printf("%s", &tampil);
+	}
+		fclose(in);
+}
+
+void menuPanel(char* judul, char* pesan)
+{
+    system("cls");
+    printf("%s\n============================================\n%s", judul, pesan);
+}
+
+void menuOpsi(char* opsi)
+{
+    printf("\n%s\n", opsi);
+}
+
+void menuPilihan(char* perintahInput)
+{
+    printf("\n%s: ", perintahInput);
+}
+
+void pesanInvalid(char* pesan)
+{
+    if (!validInput) 
+    {
+        printf("\n%s", pesan);
+        validInput = true;
+    }
+}
+
+void pilihan(char* input)
+{
+    char temp;
+    fflush(stdin);
+    scanf(" %c", &temp);
+    *input = toupper(temp);
+}
+
+void pilihanYaTidak(void (*pilih_ya)(void), void (*pilih_tidak)(void))
+{
+    while (true)
+    {
+        if (kbhit)
+        {
+            fflush(stdin);
+            char input = getch();
+            if ((int)input == KEY_ENTER) return (*pilih_ya)();
+            else if ((int) input == KEY_ESCAPE) return (*pilih_tidak)();
+        }
+    }
+}
+
+void aturPermainan()
+{
+    char input = '\000';
+    menuPanel("Mulai Permainan", "Tekan Enter untuk memulai permainan atau 'Esc' untuk kembali\n");
+    pilihanYaTidak(biarkan, bagianPermainan);
+}
+
+void bagianPermainan()
+{
+    char input = '\000';
+	
+    menuPanel("Mulai Permainan", "Pilihlah jenis permainan yang akan anda mainkan");
+    menuOpsi("1. Permainan Baru\n2. Lanjutkan Permainan Sebelumnya\nQ: Kembali");
+    menuPilihan("Masukkan Pilihan Anda");
+    pilihan(&input);
+    //if (input == 'Q')
+    int pilihanInt = atoi(&input);
+    switch (pilihanInt)
+    {
+    case 1:
+        // permainan baru
+        break;
+    case 2:
+        // permainan lama
+        //aturPermainanLama();
+        break;
+    default:
+        validInput = false;
+		pesanInvalid("Pilihan anda tidak valid");
+		getch();
+        bagianPermainan();
+        break;
+    }
+    aturPermainan();
+    mulaiPermainan();
+}
+void mulaiPermainan()
+{
+
+}
+
+void modeMultiPemain()
+{
+
+}
+
+void modeKomputer()
+{
+	int pilihan;
+
+	system("CLS");
+	
+	papan.pemain[1].isKomputer = true;
+
+	printf("Memilih Tingkat Kesulitan\n");
+	printf("Pilihlah tingkat kesulitan\n");
+	printf("[1] Mudah\n");
+	printf("[2] Sedang\n");
+	printf("[3] Sulit\n");
+	pesanInvalid("Pilihan anda tidak valid\n");
+	printf("Masukan pilihan anda:\n");
+	
+	scanf("&d", &pilihan);
+
+	memset(papan.pemain[0].nama, '\000', 50);
+	memset(papan.pemain[1].nama, '\000', 50);
+	memcpy(papan.pemain[0].nama, "Anda", 4);
+	memcpy(papan.pemain[1].nama, "Komputer", 9);
+	
+	switch (pilihan)
+	{
+	case 1 :
+		return;
+	case 2 :
+		return;
+	case 3 :
+		return;
+	default:
+		validInput = false;
+		modeKomputer();
+		break;
+	}
+}
+
+void SimpanSkor(int *skor){
+
+	FILE *in;
+	int pilihan;
+	
+	in = fopen("Daftar10SkorTertinggi.txt", "ab");
+	printf("Skor kamu : %d", skor);
+	//papan.pemain[0].skor = skor;
+
+	printf("Masukkan nama anda : ");
+	scanf("%[^\n]", &papan.pemain[0].nama[50]);
+	
+	fprintf(in, "%s - %d\n", papan.pemain[0].nama[50], papan.pemain[0].skor);
+	fclose(in);	
+}
+
+void bagianSkor()
+{
+	system("CLS");
+	printf ("\t\t\t\t\tDaftar 10 Besar Skor\n");
+	tampilSkor();
+
+	
+	printf("\n");
+	printf("\nTekan tombol apapun untuk kembali ke Menu Utama");
+	getch();
+	menuUtama();
+	
+}
 
 void bagianCaraMain()
 {
-    printf("Test");
+	char menu;
+	system("CLS");
+	FILE *in;
+	char tampil[6000];
+	
+	if((in=fopen("D:\\Kuliah\\Semester 1\\Dasar Dasar Pemograman\\Tugas Besar\\TicTacToe-Game\\CaraBermain.txt", "r")) == NULL){
+		printf("Error Opening File"); //tutup program karena file ga ada
+		fclose(in);
+	}
+		//baca isi file dengan gets lalu simpan ke tampil
+	while(fgets(tampil, sizeof(tampil), in)){
+		printf("%s", &tampil);
+	}
+		fclose(in);
+		printf("\n");
+		printf("\nTekan tombol apapun untuk kembali ke Menu Utama");
+		while (true){
+			fflush(stdin);
+			char input = getch();
+			if((int)input) return menuUtama();
+		}
 }
 
-int main(int argc, char *argv[])
+void bagianKeluar()
 {
-	printf("Hello world.");
+	exit(1);
+}
+
+void menuUtama()
+{
+	int pilihan;
+
+	system("CLS");
+		
+	printf("Menu Utama\n");
+	printf("Berikut merupakan pilihan yang dapat anda pilih.(Pilihlah pilihan dalam bentuk angka)\n");
+	printf("[1.]Mulai Permainan\n");
+	printf("[2.]Daftar 10 Pemain dengan Skor Tertinggi\n");
+	printf("[3.]Cara Bermain\n");
+	printf("[4.]Keluar Permainan\n");
+	pesanInvalid("Pilihan anda tidak valid\n");
+	printf("Masukan Pilihan Anda: ");
+	
+    scanf ("%d", &pilihan);
+	getchar();
+    
+	switch (pilihan)
+    { 
+    case 1 :
+        bagianPermainan();
+        break;
+    case 2 :
+        bagianSkor();
+        break;
+    case 3 :
+        bagianCaraMain();
+        break;
+    case 4 :
+        bagianKeluar ();
+        break;
+    default :
+		validInput = false;
+		menuUtama();
+		break;
+	}
+}
+
+int main()
+{
+	menuUtama();
 	return 0;
 }
