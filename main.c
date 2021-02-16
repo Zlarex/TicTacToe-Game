dd80731558bd005960e22f45cc8d3739cc51d4bc
Ihsan Fauzan <ihsanfauzan76@gmail.com>
2021-02-16 12:57:02 +0700

    v1.3.3 fix: minimax depth

--- main.c
+++ main.c
@@ -1144,12 +1144,12 @@ int getJangkauan(int kesulitan, int ukuran)
     {
     case 2: // sedang
         if (ukuran == 3) return 2;
-        if (ukuran == 5) return 3;
-        if (ukuran == 7) return 4;
+        if (ukuran == 5) return 2;
+        if (ukuran == 7) return 2;
     case 3: // sulit
         if (ukuran == 3) return 4;
-        if (ukuran == 5) return 5;
-        if (ukuran == 7) return 6;
+        if (ukuran == 5) return 4;
+        if (ukuran == 7) return 3;
     default: return 0;
     }
 }
